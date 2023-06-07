a0a965fa9c2496c99aa5556d43d7da9afbb8ca07
Titus <dev@tituscoleman.com>
2023-06-07 14:29:20 -0400

    [red] returns FizzBuzz check exits too early

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,9 +1,12 @@
 
 
 export function fizzBuzz (input: number): string {
-    if (input % 3 === 0)  {return "Fizz"; 
-} else if (input % 5 === 0) {
-        return "Buzz"
+    if (input % 3 === 0)  {
+        return "Fizz"; 
+    } else if (input % 5 === 0) {
+        return "Buzz";
+    } else if ((input % 3 === 0) && (input % 5 === 0)) {
+        return "FizzBuzz";
     }
     return input.toString();
 
