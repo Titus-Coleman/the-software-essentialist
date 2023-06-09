4041492a1d9db685777beb4a4e180fc143c08b1c
Titus <dev@tituscoleman.com>
2023-06-09 14:34:25 -0400

    [green] Password length check test between 5 and 15 characters

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,7 +1,7 @@
 import {PasswordValidator} from "./index"
 describe('password validator', () => {
 
-  test("Password is under characters long - invalid", () => {
+  test("Password is under 5 characters long - invalid", () => {
     let passwordValidator = new PasswordValidator("hello");
     expect(passwordValidator.isValidLength()).toBeFalsy();
   })
@@ -9,6 +9,11 @@ describe('password validator', () => {
   test("Password is over 15 characters long - invalid", () => {
     let passwordValidator = new PasswordValidator("hello20990e120e909201109");
     expect(passwordValidator.isValidLength()).toBeFalsy();
+  })
+
+  test("Password is between 5 and 15 characters long - valid", () => {
+    let passwordValidator = new PasswordValidator("helloworld");
+    expect(passwordValidator.isValidLength()).toBeTruthy();
   })
 })
 
