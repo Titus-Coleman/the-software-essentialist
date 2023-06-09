e3e997be5b462e3ac6232cbb8725765975e029d4
Titus <dev@tituscoleman.com>
2023-06-08 21:45:55 -0400

    [green] forEach - Palindromes return true

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -2,33 +2,40 @@ import { palindromeChecker } from "./index"
 
 
 describe("Palindrome Checker", () => {
-    it('"mom" returns true', () =>{
-        expect(palindromeChecker("mom")).toBeTruthy()
+
+    it("Palindromes return true", () => {
+    ["mom", "Mom", "MoM", "Never Odd or Even","1Never Odd or Even1"]
+        .map((word) => palindromeChecker(word))
+        .forEach((m) => expect(m).toBeTruthy())
     })
 
-    it('"Mom" returns true', () =>{
-        expect(palindromeChecker("Mom")).toBeTruthy()
-    })
+    // it('"mom" returns true', () =>{
+    //     expect(palindromeChecker("mom")).toBeTruthy()
+    // })
 
-    it('"MoM" returns true', () =>{
-        expect(palindromeChecker("MoM")).toBeTruthy()
-    })
+    // it('"Mom" returns true', () =>{
+    //     expect(palindromeChecker("Mom")).toBeTruthy()
+    // })
+
+    // it('"MoM" returns true', () =>{
+    //     expect(palindromeChecker("MoM")).toBeTruthy()
+    // })
 
     it('"Momx" returns false', () =>{
         expect(palindromeChecker("Momx")).toBeFalsy()
     })
 
-    it('"xMomx" returns true', () =>{
-        expect(palindromeChecker("xMomx")).toBeTruthy()
-    })
+    // it('"xMomx" returns true', () =>{
+    //     expect(palindromeChecker("xMomx")).toBeTruthy()
+    // })
 
-    it('"Was It A Rat I Saw" returns true', () =>{
-        expect(palindromeChecker("Was It A Rat I Saw")).toBeTruthy()
-    })
+    // it('"Was It A Rat I Saw" returns true', () =>{
+    //     expect(palindromeChecker("Was It A Rat I Saw")).toBeTruthy()
+    // })
 
-    it('"Never Odd or Even" returns true', () =>{
-        expect(palindromeChecker("Never Odd or Even")).toBeTruthy()
-    })
+    // it('"Never Odd or Even" returns true', () =>{
+    //     expect(palindromeChecker("Never Odd or Even")).toBeTruthy()
+    // })
 
     it('"Never Odd or Even1" returns false', () =>{
         expect(palindromeChecker("Never Odd or Even1")).toBeFalsy()
